8126166b0b668900768b93fb56a4a090b3c37c0e
Estefano Timoteo <estefano.juliano@gmail.com>
2020-05-02 16:26:24 -0300

    Navbar added as well as basic css

---
+++ src/components/Home/Info.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Title from '../Globals/Title'
+
+export default function Info() {
+  return (
+    <section className='py-5' >
+      <div className='container'>
+        <Title title='our story' />
+        <div className='row'>
+          <div className='col-10 col-sm-8 mx-auto text-center' >
+            <p className='lead text-muted mb-5'>
+              lorem daod saoid asiod asiod ais dpas dpoas dpoias pdos apodi aspood psaoi dpa dposa pdois apodi spai
+            </p>
+            <Link to='/about/'>
+              <button className='btn text-uppercase btn-yellow'>about page</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -12,10 +12,13 @@ import { useStaticQuery, graphql } from "gatsby"
 import './bootstrap.min.css'
 import "./layout.css"
 
+import Navbar from './Globals/Navbar'
+
 const Layout = ({ children }) => {
   return (
   <>
-  {children}
+    <Navbar />
+    {children}
   </>
   )
 }
