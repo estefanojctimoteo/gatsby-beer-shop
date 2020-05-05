05bf224d6e1a69d14fb2b72adc9cc2a20b670647
Estefano Timoteo <estefano.juliano@gmail.com>
2020-05-04 23:51:14 -0300

    added filtering by category

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -2,25 +2,72 @@ import React, { Component } from 'react'
 import Title from '../Globals/Title'
 import Img from 'gatsby-image'
 
+const getCategories = items => {
+  let tempItems = items.map(item => {
+    return item.node.category
+  })
+  let tempCategories = new Set(tempItems)
+  let categories = Array.from(tempCategories)
+  categories = ['all',...categories]
+  return categories
+}
+
 export default class Menu extends Component {
   constructor(props) {
     super(props)
     this.state = {
       items: props.items.edges, //[],
       beerItems: props.items.edges,
+      categories: getCategories(props.items.edges)
+    }
+    //console.log('props.items.edges: ', props.items.edges)
+    this.handleItems = this.handleItems.bind(this)
+  }
+  handleItems = (category) => {
+    console.log('category: ', category)
+    let tempItems = [...this.state.items]
+    if (category === 'all') {
+      this.setState({
+        ...this.state,
+        beerItems: tempItems,
+      })
+    }
+    else {
+      let items = tempItems.filter(({ node }) =>
+        node.category === category
+      )
+      this.setState({
+        ...this.state,
+        beerItems: items,
+      })
     }
   }
   render() {
-    const { items } = this.state
+    const { items, beerItems, categories } = this.state
+    //console.log('categories: ',categories)
     if (items && items.length > 0) {
       return (
         <section className='menu py-5' >
           <div className='container'>
             <Title title='best of our menu'></Title>
             {/* categories */}
+            <div className='row mb-5'>
+              <div className='col-10 mx-auto text-center'>
+                {categories.map((category,index) => {
+                  return (
+                    <button 
+                      type='button' 
+                      key={index}
+                      className='btn btn-yellow text-capitalize m-3'
+                      onClick={() => {this.handleItems(category)}}
+                    >{category}</button>
+                  )
+                })}
+              </div>
+            </div>
             {/* items */}
             <div className='row'>
-            {this.state.beerItems.map(({node}) => {
+            {beerItems.map(({node}) => {
               return (
                 <div key={node.id}
                   className='col-11 col-md-6 my-3 d-flex mx-auto'                  
